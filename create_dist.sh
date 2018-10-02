8b131844e035388864d9926afba162ce5d0f3527
Daniel Newton <djpnewton@gmail.com>
2018-10-02 17:55:17 +1300

    fix missing text resources in pyinstaller

--- create_dist.sh
+++ create_dist.sh
@@ -8,3 +8,6 @@ set -e
 # create 
 venv/bin/pip install pyinstaller
 venv/bin/pyinstaller zap_asset.py
+# pyinstaller misses these text resources
+mkdir -p dist/zap_asset/mnemonic
+cp -r venv/lib/python3.7/site-packages/mnemonic/wordlist dist/zap_asset/mnemonic
